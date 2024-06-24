3aa5c57d032b02466dfdf7a4e13b3f6a7ecb08ee
Ujjawal Kumar <101943673+IamUjju@users.noreply.github.com>
2024-06-24 23:24:52 +0530

    Add files via upload

--- header.component.ts
+++ header.component.ts
@@ -6,11 +6,21 @@ import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/cor
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnChanges,  OnInit {
-  @Input() message: string ='';
+  // @Input() message: string ='';
   ngOnInit(): void{
     console.log("NgOnInit");
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log("ngOnChanges");
+  }
+  count: number = 0;
+  increment(count:number): void {
+    this.count = count + 1;
+  }
+  decrement(count:number): void {
+    this.count = count - 1;
+  }
+  onSearchChange(eventData:Event):void{
+    this.count = Number(((<HTMLInputElement>eventData.target).value));
   }
 }
